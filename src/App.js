e649abfa71dacba5c1ad3d548b3da45fdbd13ae2
Alan Saldivar <alansaldivar.87@gmail.com>
2020-03-18 05:13:16 -0700

    Eslint and Prettier added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Person from './components/Person/Person'
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Person
-          name="Alan"
-          age="32"
-        />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				<Person
+					name="Alan"
+					age="32"
+				/>
+			</div>
+		)
+	}
 }
 
 export default App
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const person = (props) => {
-    const {
-        name,
-        age
-    } = props
+const person = props => {
+	const {
+		name,
+		age
+	} = props
 
-    return (
-        <div>
-            My name is { name } and my age is { age }
-        </div>
-    )
+	return (
+		<div>
+			My name is {name} and my age is {age}
+		</div>
+	)
 }
 
 export default person
