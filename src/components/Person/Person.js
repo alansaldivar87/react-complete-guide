b40872901b02bfabec44fd6bfb5eeb3ee3f1d457
Alan Saldivar <alansaldivar.87@gmail.com>
2020-03-19 00:46:29 -0700

    Add proptypes dependency

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import PropTypes from "prop-types"
+import classNames from 'classnames'
 
 const person = props => {
 	const {
 		name,
-		age
+		age,
+		classname
 	} = props
 
 	return (
-		<div>
-			My name is {name} and my age is {age}
+		<div
+			className={
+				classNames(
+					classname,
+					'person'
+				)
+			}
+		>
+			My name is { name } and my age is { age }
 		</div>
 	)
+}
+
+person.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number,
+	classname: PropTypes.string
 }
 
 export default person
