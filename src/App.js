e8916af7d05acec7b0a68f32efeb84447379eb09
Alan Saldivar <alansaldivar.87@gmail.com>
2020-03-22 04:21:21 -0700

    Basic loop and state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Person from './components/Person'
 import './App.css'
 
+const ONE = 1
+
 class App extends Component {
+	state = {
+		persons: [
+			{ id: 1, name: 'Jhon Doe', age: 27 },
+			{ id: 2, name: 'Jane Foo', age: 24 },
+			{ id: 3, name: 'Jim Fig', age: 32, children: <p>Hello</p> }
+		]
+	}
 	render() {
+		const {
+			persons
+		} = this.state;
+
 		return (
 			<div className="App">
-				<Person
-					name="Alan"
-					age={ 32 }
-				/>
-				<hr/>
-				<Person
-					name="Monica"
-					age={ 35 }
-				>
-					<p>Children example</p>
-				</Person>
+				{ persons && persons.map(({
+					id,
+					name,
+					age,
+					children
+				}, index) => (
+					<Fragment
+						key={ id }
+					>
+						<Person
+							name={ name }
+							age={ age }
+						>
+							{ children }
+						</Person>
+						{ index < persons.length - ONE && <hr/> }
+					</Fragment>
+				)) }
 			</div>
 		)
 	}
